a295853ed375cc04e3634cba991044fa6aacb5be
Christoph Gruener <christophgruener@me.com>
2019-05-28 20:44:16 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -20,3 +20,7 @@ let sally = { name: 'Sally' };
 
 greet.call(sally);
 greet.apply(sally);
+
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
